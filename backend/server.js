9a22024fdb3749d1eca7b6073a090406ae1455b4
Tresor Yves <ynsabima@andrew.cmu.edu>
2025-08-10 09:27:20 +0200

    Allow cors connection from vercel

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,8 @@ const path = require('path');
 
 // Allow requests from your frontend origin
 app.use(cors({
-  origin: 'http://localhost:3000',
-  // origin: 'https://ekima-gamification-module.vercel.app', // Uncomment this line for production
+  // origin: 'http://localhost:3000',
+  origin: 'https://ekima-gamification-module.vercel.app', // Uncomment this line for production
   credentials: true // if you use cookies/sessions
 }));
 
